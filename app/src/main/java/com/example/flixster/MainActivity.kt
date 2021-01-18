77449e2e5a29ea55c6718f56daea1410705702e3
Ernesto Diaz <ediaz188@fiu.edu>
2021-01-18 12:08:10 -0500

    performs get api request

--- app/src/main/java/com/example/flixster/MainActivity.kt
+++ app/src/main/java/com/example/flixster/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.flixster
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
+import org.json.JSONException
 
+const val NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"
+const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val client = AsyncHttpClient()
+        client.get(NOW_PLAYING_URL, object: JsonHttpResponseHandler() {
+            override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
+                Log.d(TAG, "onSuccess")
+                val jsonObject = json.jsonObject
+                try {
+                    val results = jsonObject.getJSONArray("results")
+                    Log.i(TAG, "Results: $results")
+                } catch (e: JSONException) {
+                    Log.e(TAG, "Hit json exception", e)
+                }
+            }
+
+            override fun onFailure(statusCode: Int, headers: Headers, response: String, throwable: Throwable) {
+                Log.d("MainActivity", "onFailure")
+            }
+        })
     }
 }
