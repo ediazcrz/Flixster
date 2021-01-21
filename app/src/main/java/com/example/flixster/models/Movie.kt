22cb74075d9f37f48e3957c4e1a420964ec918f7
Ernesto Diaz <ediaz188@fiu.edu>
2021-01-20 20:33:26 -0500

    added backdrop path to Movie model

--- app/src/main/java/com/example/flixster/models/Movie.kt
+++ app/src/main/java/com/example/flixster/models/Movie.kt
@@ -3,6 +3,9 @@ package com.example.flixster.models
 import com.google.gson.annotations.SerializedName
 
 data class Movie(
+    @SerializedName("backdrop_path")
+    val backdropPath: String,
+
     @SerializedName("poster_path")
     val posterPath: String,
 
