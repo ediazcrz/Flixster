d1b8eae8bf75289f58fbe5b05f518f74f38c01b2
Ernesto Diaz <ediaz188@fiu.edu>
2021-01-20 20:32:55 -0500

    modifies the poster when the orientation changes

--- app/src/main/java/com/example/flixster/adapters/MovieAdapter.kt
+++ app/src/main/java/com/example/flixster/adapters/MovieAdapter.kt
@@ -1,6 +1,8 @@
 package com.example.flixster.adapters
 
 import android.content.Context
+import android.content.res.Configuration
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -10,13 +12,18 @@ import com.example.flixster.adapters.MovieAdapter.MovieViewHolder
 import com.example.flixster.databinding.ItemMovieBinding
 import com.example.flixster.models.Movie
 
+const val TAG = "MovieAdapter"
+const val baseUrl = "http://image.tmdb.org/t/p/"
+
 class MovieAdapter(private val context: Context, private val movies: List<Movie>): RecyclerView.Adapter<MovieViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        Log.d(TAG, "onCreateViewHolder")
         val view = ItemMovieBinding.inflate(LayoutInflater.from(context), parent, false)
         return MovieViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        Log.d(TAG, "onBindViewHolder $position")
         val movie: Movie = movies[position]
         holder.bind(movie)
     }
@@ -29,8 +36,17 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
         fun bind(movie: Movie) {
             binding.tvTitle.text = movie.title
             binding.tvOverview.text = movie.overview
+
+            val orientation = context.resources.configuration.orientation
+
+            val imageUrl = if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                "$baseUrl/w1280/${movie.backdropPath}"
+            } else {
+                "$baseUrl/w342/${movie.posterPath}"
+            }
+
             Glide.with(binding.root)
-                    .load("https://image.tmdb.org/t/p/w342/${movie.posterPath}")
+                    .load(imageUrl)
                     .centerInside()
                     .into(binding.ivPoster);
         }
