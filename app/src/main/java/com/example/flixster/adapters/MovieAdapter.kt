639a3847c24ddf8afdc1c2d47920874875ce29f7
Ernesto Diaz <ediaz188@fiu.edu>
2021-01-28 22:30:04 -0500

    optimized imports

--- app/src/main/java/com/example/flixster/adapters/MovieAdapter.kt
+++ app/src/main/java/com/example/flixster/adapters/MovieAdapter.kt
@@ -5,10 +5,7 @@ import android.content.Intent
 import android.content.res.Configuration
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.bumptech.glide.Glide
